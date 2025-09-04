052e82e1939b567bab9f351c6b509f77cdce729d
Reynir Björnsson <reynir@reynir.dk>
2025-09-04 14:17:01 +0200

    Expand conv example
    It turns out an exception is not exactly treated as a non-match. It will
    make subsequent routes non-match, too :/

--- examples/conv/conv.ml
+++ examples/conv/conv.ml
@@ -1,18 +1,27 @@
-let default req number _server () =
-  let field = "content-type" in
+let field = "content-type"
+
+let number req number _server () =
   let open Vif.Response.Syntax in
-  let* () = Vif.Response.add ~field "text/html; charset=utf-8" in
+  let* () = Vif.Response.add ~field "text/plain; charset=utf-8" in
   let* () =
     Printf.ksprintf (Vif.Response.with_string req)
       "Hello World! The number is %u!\n" number
   in
   Vif.Response.respond `OK
 
+let horse req _server () =
+  let open Vif.Response.Syntax in
+  let* () = Vif.Response.add ~field "text/plain; charset=utf-8" in
+  let* () = Vif.Response.with_string req "The horse goes prrrrr!\n" in
+  Vif.Response.respond `OK
+
+
 let routes =
   let open Vif.Uri in
   let open Vif.Route in
   let bad_int = conv int_of_string string_of_int (string `Path) in
-  [ get (rel /% bad_int /?? nil) --> default ]
+  [ get (rel /% bad_int /?? nil) --> number
+  ; get (rel / "horse" /?? nil) --> horse ]
 
 let () =
   Miou_unix.run @@ fun () ->
